1c84ca23d7ff7836e5a3f3db68025feefc11c31b
Nasir uddin <nasiruddin@thriwe.com>
2020-04-17 16:16:31 +0530

    ++bk

--- common/config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'adminEmail' => 'donoreply@growelimpex.com',
-    'supportEmail' => 'donoreply@growelimpex.com',
-    'senderEmail' => 'donoreply@growelimpex.com',
-    'senderName' => 'donoreply@growelimpex.com',
-    'user.passwordResetTokenExpire' => 3600,
-];
--- frontend/web/html/application/views/index.php
+++
@@ -1,3 +0,0 @@
-<?php include'include/header.php';?>
-  <?php echo $main_content;?>
-<?php include'include/footer.php';?>
